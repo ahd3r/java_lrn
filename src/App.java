2e1d455eb509e43e02911678242c663b7d9716ab
Ander Stac <37026127+ahd3r@users.noreply.github.com>
2024-03-01 13:35:22 -0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,6 +5,17 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
+        int res = 0;
+        for (int y = 0; y < 15; y++) {
+            res = 0;
+            for (int i = 0; i < 2147483647; i++) {
+                res += i;
+            }
+        }
+        System.out.println("Main thread finished");
+        System.out.println(res);
+        System.out.println("Other threads are about to start");
+
         List<Thread> threads = new ArrayList<>();
         List<Calculate> classes = new ArrayList<>();
 
